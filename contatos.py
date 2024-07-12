9d86aba0df4766c83b24bbd22ca38698b81528aa
fernando laudelino <nandoverananda@gmail.com>
2024-07-12 17:38:46 -0300

    Adicionei o campo id na agenda

--- contatos.py
+++ contatos.py
@@ -29,19 +29,18 @@ def ExcluirNome(nome):
                 conecao.rollback()
                 print(f'Nome {nome} não encontrado')
 def LerDados():
-    contatos=[]
     with sqlite3.connect('agenda.db') as conecao:
         with closing(conecao.cursor()) as curso:
             curso.execute('select * from cadastro')
-            '''print(f'{"Nome":^20}|{"Telefone":^13}')
-            print('=================================')'''
+            print(f'{"id":<4}|{"Nome":<20}|{"Telefone":^13}')
+            print('=================================')
             while True:
                 resultado = curso.fetchone()
                 if resultado is None:
                     break
-                #print(f'{resultado[0]:<20}|{resultado[1]:<13}')
-                contatos.append(resultado)
-    return contatos
+                print(f'{resultado[0]:^4}|{resultado[1]:<20}|{resultado[2]:<13}')
+
+
 
 
 
@@ -49,13 +48,13 @@ def BascarNome(nome):
     with sqlite3.connect('agenda.db') as conecao:
         with closing(conecao.cursor()) as curso:
             curso.execute(F"select * from cadastro where nome = '{nome}'")
-            print(f'{"Nome":^20}|{"Telefone":^13}')
+            print(f'{"id":^4}|{"Nome":^20}|{"Telefone":^13}')
             print('=================================')
             while True:
                 resultado = curso.fetchone()
                 if resultado is None:
                     break
-                print(f'{resultado[0]:<20}|{resultado[1]:<13}')
+                print(f'{resultado[0]:^4}|{resultado[1]:<20}|{resultado[2]:<13}')
 
 def NumeroTell(nome):
     with sqlite3.connect('agenda.db') as conecao:
@@ -65,7 +64,7 @@ def NumeroTell(nome):
                 resultado = curso.fetchone()
                 if resultado is None:
                     break
-                reslt = resultado[1]
+                reslt = resultado[2]
     return reslt
 
 if __name__=='__main__':
@@ -73,4 +72,5 @@ if __name__=='__main__':
     #InserirDados("Fernando","11945922382")
     #LerDados()
     #ExcluirNome('Fernando')
-    print(NumeroTell('Renan'))
+    #print(NumeroTell('Renan'))
+    BascarNome('Fernando')
